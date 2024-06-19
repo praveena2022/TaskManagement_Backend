d7fe39e5f27102c95baa64b7246c4acf8cb6ac8f
Praveena2022 <gpraveena8523@gmail.com>
2024-06-19 15:56:03 +0530

    azure credentials chnged

--- src/test/java/com/revtaskmanagement/RevTask/Service/ProjectServiceTest.java
+++ src/test/java/com/revtaskmanagement/RevTask/Service/ProjectServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -21,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class ProjectServiceTest {
 
     @MockBean
