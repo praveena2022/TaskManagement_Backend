c136edac9a8980ac0ec702e128da2d07b2e56037
Praveena2022 <gpraveena8523@gmail.com>
2024-06-20 14:20:21 +0530

    web config chnaged

--- src/main/java/com/revtaskmanagement/RevTask/Config/WebConfig.java
+++ src/main/java/com/revtaskmanagement/RevTask/Config/WebConfig.java
@@ -7,13 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("https://delightful-plant-046ea7d0f.5.azurestaticapps.net")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allows CORS for all endpoints
                 .allowedOrigins("https://delightful-plant-046ea7d0f.5.azurestaticapps.net")  // Allowed origin
